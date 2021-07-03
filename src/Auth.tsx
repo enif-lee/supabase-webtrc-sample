b99d0459b4470508677784e0dd0a9c169c5558ae
yongsang <restart916@gmail.com>
2021-07-03 17:34:31 +0900

    이메일로 회원가입 하는 기능 추가

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,43 +1,68 @@
-// import { createClient } from '@supabase/supabase-js';
-import React, { useCallback } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import React from 'react';
 
-// const supabase = createClient(
-//   'https://aqquavrzsoekswmysfsl.supabase.co',
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNDk3OTY0MCwiZXhwIjoxOTQwNTU1NjQwfQ.glKMPY33SD0qu0H2_NmmuZubGi-sMBaDMYlVLgsRqZs',
-// );
+const supabase = createClient(
+  'https://aqquavrzsoekswmysfsl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNDk3OTY0MCwiZXhwIjoxOTQwNTU1NjQwfQ.glKMPY33SD0qu0H2_NmmuZubGi-sMBaDMYlVLgsRqZs',
+);
 
-function Auth() {
-  // const { loading, value } = useAsync(async () => {
-  //   const { error, body } = await supabase.from('chats').select();
-  //   if (error) throw error;
-  //   return body;
-  // });
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
 
-  const testClick = useCallback(async () => {
-    // await supabase
-    //   .from('chats')
-    //   .update({
-    //     answer: {
-    //       type: answerDescription.type,
-    //       sdp: answerDescription.sdp,
-    //     },
-    //   })
-    //   .match({ id: 1 });
-  }, []);
+    this.handleChange = this.handleChange.bind(this);
+    this.testClick = this.testClick.bind(this);
+  }
 
-  // useLayoutEffect(() => {
-  //   fetch();
-  // }, []);
+  async testClick() {
+    let { user, error } = await supabase.auth.signUp({
+      email: this.state.email,
+      password: this.state.password,
+    });
 
-  return (
-    <div>
-      <div>Auth</div>
+    if (error) {
+      console.log('error', error);
+      alert(`회원가입 실패! ${error.message}`);
+      return;
+    }
 
+    console.log('user', user);
+    alert('회원가입 성공! 인증 이메일이 발송되었습니다');
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  render() {
+    return (
       <div>
-        <button onClick={testClick}>로그인테스트</button>
+        <div>Auth</div>
+
+        <div>
+          <input
+            id="email"
+            type="text"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            id="password"
+            type="text"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <button onClick={this.testClick}>가입 테스트</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Auth;
